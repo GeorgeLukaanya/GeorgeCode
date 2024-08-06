17697a14cfd6baa088f82c40ba3832ff4277b0d6
GeorgeLukaanya <georgelukaanya348@gmail.com>
2024-08-06 17:05:23 +0300

    New Python

---
+++ python/CS50 Introduction to Programming/Libraries/generate.py
@@ -0,0 +1,13 @@
+#The random module
+from random import choice, randint, shuffle
+
+coin = choice(["heads", "tails"])
+print(coin)
+
+number = randint(1, 10)
+print(number)
+
+cards = ["jack", "queen", "king"] 
+shuffle(cards)
+for card in cards:
+    print(cards)

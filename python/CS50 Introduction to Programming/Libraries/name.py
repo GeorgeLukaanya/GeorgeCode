0bc87a07a2c952a72144149541aab4f87558aaea
GeorgeLukaanya <georgelukaanya348@gmail.com>
2024-08-06 17:15:31 +0300

    New Python

--- python/CS50 Introduction to Programming/Libraries/name.py
+++ python/CS50 Introduction to Programming/Libraries/name.py
@@ -1,6 +1,10 @@
 import sys
 
-try:
-    print("Hello, my name is ", sys.argv[1])
-except IndexError:
-    print("Too few arguments")
+if len(sys.argv) < 2:
+    sys.exit("too few arguments")
+    
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+#else:
+
+print(f"Hello, my name is {sys.argv[1]}")

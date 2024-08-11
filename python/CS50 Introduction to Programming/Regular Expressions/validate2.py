5fc50c260a4de2b1b49cff82444a27bbcf41093e
GeorgeLukaanya <georgelukaanya348@gmail.com>
2024-08-11 23:19:13 +0300

    New Python4

--- python/CS50 Introduction to Programming/Regular Expressions/validate2.py
+++ python/CS50 Introduction to Programming/Regular Expressions/validate2.py
@@ -1,12 +1,17 @@
-import re
+import re  
 
-email = input("What's your email? ")
+email = input("What's your email? ").strip()
 
 #.+@.+ = ..*@..*
-#r for a raw string
+#r for a raw string 
 #^[^@]+@[^@]+\.edu$
+#^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$
+#^\w+@\w+\.edu$
+#[a-zA-Z0-9_] = (\w|\s) which caters for a whitespace too
+#(\w+\.)? 0 or 1 repetition
+#^\w+@(\w+\.)?\w+\.(com|edu|gov|net|org)$
  
-if re.search(r"^[^@]+@[^@]+\.edu$" , email):  
+if re.search(r"^(\w|\.)+@(\w+\.)?\w+\.(com|edu|gov|net|org)$" , email, flags=re.IGNORECASE):  
     print("Valid")
 else:
-    print("Invalid")
+    print("Invalid") 

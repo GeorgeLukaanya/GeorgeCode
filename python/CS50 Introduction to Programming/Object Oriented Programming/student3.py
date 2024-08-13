79ca8a86c094bc6d9bdebbd02b1b0cc4817385cc
GeorgeLukaanya <georgelukaanya348@gmail.com>
2024-08-13 08:46:19 +0300

    Using __init__ and __str__

--- python/CS50 Introduction to Programming/Object Oriented Programming/student3.py
+++ python/CS50 Introduction to Programming/Object Oriented Programming/student3.py
@@ -1,23 +1,42 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
         
     def __str__(self):
+        #returns an f-string
         return f"{self.name} from {self.house}"
+    
+    #all class methods take an argument self atleast
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🐎"
+            case "Otter":
+                return "🦏"
+            case "jacl Russel terrier":
+                return "🦣"
+            case _:
+                return "🗿"
+
 
 def main():
     student = get_student()
-    print(student)
+    #calls to the __str__ method
+    print("Expecto Patronum!")
+    print(student.charm())
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 if __name__ == "__main__":
     main()
+
